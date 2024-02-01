005b87a64cd8867873c3ef5c5d98eb7373c7e035
wangbo1 <wangbo1@beisen.com>
2024-02-01 16:46:13 +0800

    初始化使用本地数据测试通过

---
+++ OpenGaussConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenGauss.NET;
+
+Console.WriteLine("Hello, World!");
+
+
+var connString = "Server=localhost;Port=5432;Username=gaussdb;Password=openGauss@123;Database=test;Timeout=60;Command Timeout=60";
+
+var connection = new OpenGaussConnection(connString);
+await connection.OpenAsync();
+await using var cmd = new OpenGaussCommand("SELECT * FROM public.userinfo", connection);
+await using var reader = await cmd.ExecuteReaderAsync();
+while (await reader.ReadAsync())
+{
+    Console.WriteLine(reader.HasRows);
+    Console.WriteLine(reader.FieldCount);
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+        Console.WriteLine(reader.GetName(i));
+        Console.WriteLine(reader.GetValue(i));
+    }
+}
+
+// var connectString = "Host=localhost;Port=5432;Username=gaussdb;Password=openGauss@123;Database=test;No Reset On Close=true;Maximum Pool Size=512;timeout = 30;";
+// using (DataSet dataSet = NpgSqlHelper.ExecuteDataset(connectString, CommandType.Text, "SELECT * FROM public.userinfo"))
+// {
+//     Console.WriteLine(dataSet.GetXml());
+// }
---
+++ README.md
@@ -0,0 +1,15 @@
+#移植华为openGauss 支持.net 8.0
+
+参考 https://gitee.com/opengauss/openGauss-connector-adonet
+
+## 1. 安装openGauss
+
+容器安装
+https://hub.docker.com/r/opengauss/opengauss
+
+本地推荐工具 使用
+DBeaver
+驱动
+https://mvnrepository.com/artifact/org.opengauss/opengauss-jdbc
+工具链接url
+例如：jdbc:postgresql://localhost:5432/test
